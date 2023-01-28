dd263f94c173091c52507f1d3d44536b9f9686ab
Aran30 <50965489+Aran30@users.noreply.github.com>
2023-01-28 12:08:49 +0100

    changed infos

--- src/app/experience/experience.ts
+++ src/app/experience/experience.ts
@@ -2,34 +2,17 @@ export const experiences: ExperienceYear[] = [
   {
     year: 2022,
     experiences: [
-      { title: 'Bachelors Degree', date: new Date('2022-02-07') },
-      {
-        title: 'Celonis Hackathon at digitalCHURCH ',
-        date: new Date('2022-04-29'),
-      },
-      {
-        title: 'Practical Lab at E.ON',
-        date: new Date('2022-08-01'),
-      },
-    ],
-  },
-  {
-    year: 2021,
-    experiences: [
-      {
-        title: 'Work as research assistant at RWTH Aachen University',
-        date: new Date('2021-01-05'),
-      },
-      {
-        title: 'Work as webmaster for the AVL academic circle',
-        date: new Date('2021-10-31'),
-      },
+      { title: 'Worked as ERSTI-Mentor at RWTH', date: new Date('2022-10-03') },
     ],
   },
   {
     year: 2020,
     experiences: [
-      { title: 'Launch of Cards webpage', date: new Date('2020-02-07') },
+      { title: 'Bachelors Degree', date: new Date('2020-10-09') },
+      {
+        title: 'Work as research at RWTH I5 ACIS group',
+        date: new Date('2020-10-09'),
+      },
     ],
   },
 ];
--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -2,18 +2,6 @@ import { Project } from '../models/project.model';
 
 export const projects: Project[] = [
   {
-    title: 'Automatic Conformance Checking',
-    previewImgSrc: 'assets/acc.png',
-    page: {
-      href: 'https://pyinsights.tech/',
-      description:
-        'A framework providing automatic conformance diagnostics on event logs uploaded to Celonis. (A Celonis account and application key is required)',
-    },
-    source: {
-      href: 'https://github.com/lakhoune/Automatic-Conformance-Checking',
-    },
-  },
-  {
     title: 'Social Bot Framework',
     subtitle: 'Bot modeling and development',
     previewImgSrc: 'assets/sbf-preview.webp',
@@ -25,28 +13,5 @@ export const projects: Project[] = [
     source: {
       href: 'https://github.com/rwth-acis/Social-Bot-Framework',
     },
-  },
-  {
-    title: 'MobSOS Evaluation Center',
-    subtitle: 'Community Analytics',
-    previewImgSrc: 'assets/mobsos-preview.webp',
-    page: {
-      href: 'https://monitor.tech4comp.dbis.rwth-aachen.de/welcome',
-      description:
-        'MobSOS is a framework for community information systems (CIS) success modeling. The goal is to increase success awareness in a community.',
-    },
-    source: {
-      href: 'https://github.com/rwth-acis/mobsos-evaluation-center',
-    },
-  },
-  {
-    title: 'StudyGrow Cards',
-    subtitle: 'Digital Flashcards',
-    previewImgSrc: 'assets/cards-preview.webp',
-    page: {
-      href: 'https://studygrow-cards.herokuapp.com/',
-      description:
-        'This application can be used to create flashcards and share them with the community',
-    },
-  },
+  }
 ];
