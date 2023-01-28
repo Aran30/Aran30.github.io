e943191ad6d2f82020e815573aef738778022e82
Aran30 <50965489+Aran30@users.noreply.github.com>
2023-01-28 13:49:57 +0100

    fuck

--- src/app/experience/experience.ts
+++ src/app/experience/experience.ts
@@ -2,29 +2,36 @@ export const experiences: ExperienceYear[] = [
   {
     year: 2022,
     experiences: [
-      { title: 'Worked as ERSTI-Mentor at RWTH', date: new Date('2022-10-03') },
+      { title: 'Bachelors Degree', date: new Date('2022-02-07') },
+      {
+        title: 'Celonis Hackathon at digitalCHURCH ',
+        date: new Date('2022-04-29'),
+      },
+      {
+        title: 'Practical Lab at E.ON',
+        date: new Date('2022-08-01'),
+      },
     ],
   },
   {
     year: 2021,
     experiences: [
-      { title: 'Bachelors Degree', date: new Date('2020-10-09') },
       {
-        title: 'Work as research at RWTH I5 ACIS group',
-        date: new Date('2020-10-09')
-      }
-    ]
+        title: 'Work as research assistant at RWTH Aachen University',
+        date: new Date('2021-01-05'),
+      },
+      {
+        title: 'Work as webmaster for the AVL academic circle',
+        date: new Date('2021-10-31'),
+      },
+    ],
   },
   {
     year: 2020,
     experiences: [
-      { title: 'Bachelors Degree', date: new Date('2020-10-09') },
-      {
-        title: 'Work as research at RWTH I5 ACIS group',
-        date: new Date('2020-10-09')
-      }
-    ]
-  }
+      { title: 'Launch of Cards webpage', date: new Date('2020-02-07') },
+    ],
+  },
 ];
 
 export interface ExperienceYear {
--- src/app/socials/socials.ts
+++ src/app/socials/socials.ts
@@ -1,11 +1,11 @@
 import { Social } from '../models/social.model';
 
 export const socials: Social[] = [
-  new Social('bi bi-github', 'https://github.com/aran30'),
-  new Social('bi bi-envelope-fill', 'mailto:conrardy.aaron30@gmail.com'),
+  new Social('bi bi-github', 'https://github.com/lakhoune'),
+  new Social('bi bi-envelope-fill', 'mailto:a.b.lakhoune@gmail.com'),
   new Social(
     'bi bi-linkedin',
-    'https://www.linkedin.com/in/aaron-david-conrardy-044893225/'
+    'https://www.linkedin.com/in/ben-lakhoune-235712223'
   ),
-  new Social('bi bi-twitter', 'https://github.com/aran30'),
+  new Social('bi bi-twitter', 'https://twitter.com/ben_lakhoune'),
 ];
