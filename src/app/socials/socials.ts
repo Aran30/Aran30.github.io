1057112392f7c2a35d6895cbbd44da2ba932bdf9
Aran30 <50965489+Aran30@users.noreply.github.com>
2023-01-28 13:08:23 +0100

    Update socials.ts

--- src/app/socials/socials.ts
+++ src/app/socials/socials.ts
@@ -6,6 +6,6 @@ export const socials: Social[] = [
   new Social(
     'bi bi-linkedin',
     'https://www.linkedin.com/in/aaron-david-conrardy-044893225/'
-  )
+  ),
   new Social('bi bi-twitter', 'https://github.com/aran30'),
 ];
