83faefa7d97ed6fe521e344bfce992e987540b56
Aran30 <50965489+Aran30@users.noreply.github.com>
2023-01-28 11:07:01 +0100

    Revert "yes"
    This reverts commit 56a26a7de3603a5b73004d5afb2b3de92e997421.

--- src/app/models/social.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Social {
-  logo: string | { src: string };
-  link: string;
-}
-
-export class Social implements Social {
-  constructor(public logo: string | { src: string }, public link: string) {}
-}
